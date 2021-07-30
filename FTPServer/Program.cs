bdeec62f38c65a818ed5286b14f91264148c4abf
nmaypeterhuang <77961515+nmaypeterhuang@users.noreply.github.com>
2021-07-30 16:05:08 +0800

    Create Program.cs

---
+++ FTPServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mike;
+
+namespace FTPServer
+{
+    class Program
+    {
+        public static Log logger = new Log();
+
+        static void Main(string[] args)
+        {
+            //設定LOG物件
+            string logName = "FTPServer." + DateTime.Now.yyyyMMdd() + "." + DateTime.Now.HHmmss();
+            logger.SetLogObject(logName);
+            logger.Info("開始執行");
+
+            try
+            {
+                MainProcess.ProgramProcess();
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorException("未預期錯誤", ex);
+            }
+        }
+    }
+}
